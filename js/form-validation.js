f771ab0e16c41f4fb334b5785da99bcf20617288
Vishal Damodar <thatvishal007@gmail.com>
2025-07-25 18:53:45 +0530

    form validation js, and some links updated

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,20 +1,19 @@
 
 (function () {
-  'use strict';
+          'use strict'
+          var forms = document.querySelectorAll('.needs-validation')
+          Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+              form.addEventListener('submit', function (event) {
+                event.preventDefault()
+                event.stopPropagation()
+                if (form.checkValidity()) {
+                  alert('Message sent successfully!');
+                  form.reset();
+                }
+                form.classList.add('was-validated')
+              }, false)
+            })
+        })()
 
- 
-  var forms = document.querySelectorAll('.needs-validation');
 
-
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add('was-validated');
-      }, false);
-    });
-})();
